f280d1e535f4557b92772896588cc6fe5502ccfd
Sadiq Umar <umarsadiqibrahim@gmail.com>
2023-11-02 04:52:29 +0100

    Start creating event page

---
+++ app/src/main/java/com/example/myeventguide/EventPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.myeventguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_page);
+    }
+}
